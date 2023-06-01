94164700daa8aab836749e17afaffdd88d4e5802
mrcbq <marcobaquero@gmail.com>
2023-06-01 00:02:57 -0500

    Add modifications to the components buttonBox adding onclick events, Add hook useState for display value in App module

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 import ButtonBox from './components/ButtonBox';
 import Screen from './components/Screen';
 import './App.css';
 
 function App() {
+  const [displayValue, setDisplayValue] = React.useState('0');
+
+  const handleButtonClick = (calculatorData) => {
+    const { total, next } = calculatorData;
+    setDisplayValue(next || total || '0');
+  };
+
   return (
     <Calculator>
-      <Screen value="0" />
-      <ButtonBox />
+      <Screen value={displayValue} />
+      <ButtonBox onButtonClick={handleButtonClick} />
     </Calculator>
   );
 }
--- src/components/ButtonBox.js
+++ src/components/ButtonBox.js
@@ -1,29 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
+import calculate from '../logic/calculate';
 import './ButtonBox.css';
 
-const ButtonBox = () => (
-  <div className="buttonBox">
-    <Button value="AC" />
-    <Button value="+/-" />
-    <Button value="%" />
-    <Button className="bg-orange" value="÷" />
-    <Button value="7" />
-    <Button value="8" />
-    <Button value="9" />
-    <Button className="bg-orange" value="x" />
-    <Button value="4" />
-    <Button value="5" />
-    <Button value="6" />
-    <Button className="bg-orange" value="-" />
-    <Button value="1" />
-    <Button value="2" />
-    <Button value="3" />
-    <Button className="bg-orange" value="+" />
-    <Button className="zero" value="0" />
-    <Button value="." />
-    <Button className="bg-orange" value="=" />
-  </div>
-);
+const ButtonBox = ({ onButtonClick }) => {
+  const [calculatorData, setCalculatorData] = React.useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    const updatedData = calculate(calculatorData, buttonName);
+    setCalculatorData(updatedData);
+    onButtonClick(updatedData);
+  };
+
+  return (
+    <div className="buttonBox">
+      <Button value="AC" onClick={() => handleButtonClick('AC')} />
+      <Button onClick={() => handleButtonClick('+/-')} value="+/-" />
+      <Button onClick={() => handleButtonClick('%')} value="%" />
+      <Button
+        onClick={() => handleButtonClick('÷')}
+        className="bg-orange"
+        value="÷"
+      />
+      <Button onClick={() => handleButtonClick('7')} value="7" />
+      <Button onClick={() => handleButtonClick('8')} value="8" />
+      <Button onClick={() => handleButtonClick('9')} value="9" />
+      <Button
+        onClick={() => handleButtonClick('x')}
+        className="bg-orange"
+        value="x"
+      />
+      <Button onClick={() => handleButtonClick('4')} value="4" />
+      <Button onClick={() => handleButtonClick('5')} value="5" />
+      <Button onClick={() => handleButtonClick('6')} value="6" />
+      <Button
+        onClick={() => handleButtonClick('-')}
+        className="bg-orange"
+        value="-"
+      />
+      <Button onClick={() => handleButtonClick('1')} value="1" />
+      <Button onClick={() => handleButtonClick('2')} value="2" />
+      <Button onClick={() => handleButtonClick('3')} value="3" />
+      <Button
+        onClick={() => handleButtonClick('+')}
+        className="bg-orange"
+        value="+"
+      />
+      <Button
+        onClick={() => handleButtonClick('0')}
+        className="zero"
+        value="0"
+      />
+      <Button onClick={() => handleButtonClick('.')} value="." />
+      <Button
+        onClick={() => handleButtonClick('=')}
+        className="bg-orange"
+        value="="
+      />
+    </div>
+  );
+};
+
+ButtonBox.propTypes = {
+  onButtonClick: PropTypes.func.isRequired,
+};
 
 export default ButtonBox;
