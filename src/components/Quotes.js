036ac1fe080cf3838aad6598dd1f6354ad1a704d
mrcbq <marcobaquero@gmail.com>
2023-06-01 21:11:34 -0500

    Add error state, Change loading state

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,48 +3,48 @@ import { useState, useEffect } from 'react';
 function Quotes() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const headersList = {
-        Accept: '*/*',
-        'X-Api-Key': 'FXOCS/tG2JzdRhoyqGV8tQ==3JGYgTMkA2vzJo1K',
-      };
-      const response = await fetch(
-        'https://api.api-ninjas.com/v1/quotes?category=men',
-        {
-          method: 'GET',
-          headers: headersList,
-        },
-      );
-      const data = await response.json();
-      setData(data);
-      setIsLoading(false);
-      // console.log(data);
+      try {
+        const headersList = {
+          Accept: '*/*',
+          'X-Api-Key': 'FXOCS/tG2JzdRhoyqGV8tQ==3JGYgTMkA2vzJo1K',
+        };
+        const response = await fetch(
+          'https://api.api-ninjas.com/v1/quotes?category=men',
+          {
+            method: 'GET',
+            headers: headersList,
+          },
+        );
+        const data = await response.json();
+        setData(data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+      }
     };
     fetchData();
   }, []);
 
   return (
-    <ul>
-      {isLoading ? (
-        <li>Loading...</li>
-      ) : (
-        <li>
-          {data.map((item) => (
-            <li key={item.category}>
-              {item.quote}
-              {' '}
-              <br />
-              {' '}
-              -
-              {' '}
-              {item.author}
-            </li>
-          ))}
-        </li>
-      )}
-    </ul>
+    <>
+      {error ? <p>{error.message}</p> : ''}
+      {isLoading ? <p>Loading...</p> : ''}
+      {data.map((item) => (
+        <p key={1}>
+          {item.quote}
+          {' '}
+          <br />
+          {' '}
+          -
+          {' '}
+          {item.author}
+        </p>
+      ))}
+    </>
   );
 }
 
