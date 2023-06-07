4bbbf8f38448744c42598ec6543390ff0bff9e4d
mrcbq <marcobaquero@gmail.com>
2023-06-06 19:04:48 -0500

    Add routes logic and imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import ButtonBox from './components/ButtonBox';
 import Screen from './components/Screen';
 import Quotes from './components/Quotes';
+import Home from './components/Home';
+import NotFoundPage from './components/NotFound';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   const [displayValue, setDisplayValue] = React.useState('0');
@@ -14,13 +18,23 @@ function App() {
   };
 
   return (
-    <>
-      <Calculator>
-        <Screen value={displayValue} />
-        <ButtonBox onButtonClick={handleButtonClick} />
-      </Calculator>
-      <Quotes />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <Calculator>
+              <Screen value={displayValue} />
+              <ButtonBox onButtonClick={handleButtonClick} />
+            </Calculator>
+          )}
+        />
+        <Route path="/quotes" element={<Quotes />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
