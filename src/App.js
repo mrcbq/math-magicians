c6f88d193d971ed65a7d442f60da0a7baa9b69de
mrcbq <marcobaquero@gmail.com>
2023-06-06 20:57:46 -0500

    Change BrowserRouter to HashRouter for deploy correctly on GH pages, add the repo basename

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
 import Home from './components/Home';
@@ -8,15 +8,15 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Calculator />} />
+        <Route path="/" element={<Calculator />} basename="/https://mrcbq.github.io/math-magicians/" />
         <Route path="/quotes" element={<Quotes />} />
         <Route path="/home" element={<Home />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
